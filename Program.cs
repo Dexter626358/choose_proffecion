d07158c0a249fcece2c6c82347506e1acf913104
Sergey <gusevsergey85@gmail.com>
2022-09-03 13:44:11 +0300

    Created function

--- Program.cs
+++ Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿String[] array = {"Java", "Python", "PHP", "GO", "Swift", "R", "3", "13", "A"};
+String[] getArray(String[] array)
+{
+    int count = 0;
+    for(int i=0; i < array.Length; i++)
+    {
+        if(array[i].Length <= 3)
+        {
+            count++;
+        }
+    }
+    String[] result = new String[count];
+    int index = 0;
+    for(int i=0; i < array.Length; i++)
+    {
+        if(array[i].Length <= 3)
+        {
+            result[index] += array[i];
+            index++;
+        }
+    }
+
+    return result;
+}
+String[] newArray = getArray(array);
+foreach(var value in newArray)
+{
+    Console.Write(value + " ");
+}
+
